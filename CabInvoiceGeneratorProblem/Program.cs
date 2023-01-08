9581f4fa43a03212570df17216f3dd5b9646bdbb
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-08 20:08:04 +0530

    [charu]welcome to cabinvoicegenerator

---
+++ CabInvoiceGeneratorProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CabInvoiceGeneratorProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Cab invoice generator problem");
+        }
+    }
+}
