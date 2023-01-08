f8df82f6765ec617bb47a6638e4d843ace1adf3b
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-08 20:23:56 +0530

    [charu]calculate total fare for single ride of normal type journey

---
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+using CabInvoiceGeneratorProblem;
+
+namespace CabInvoiceGeneratorTest
+{
+    public class InvoiceTests
+    {
+        InvoiceGenerator invoice = new InvoiceGenerator();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void givenDistanceAndTime_ShouldReturnTotalFare()
+        {
+            double distance = 8.0;
+            int time = 12;
+            double fare = invoice.calculateFare(distance, time);
+            System.Console.WriteLine("fare is : " + fare);
+            Assert.AreEqual(92, fare, 0.0);
+        }
+    }
+}
